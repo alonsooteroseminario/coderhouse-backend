d2bcb10bc6d406001a839a011e5eb2d9fa3c833a
Luis Otero <alonsooteroseminario@gmail.com>
2021-04-20 22:26:48 -0400

    actualiado al after

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const productRoutes = require("./routes/products");
-
-let PRODUCTS_DB = [];
+const frontRoutes = require('./routes/front');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use("/api/productos", productRoutes);
+
+app.use("/web", frontRoutes);
 
 const server = app.listen(port, () => {
   console.log('El servidor esta corriendo en el puerto: ' + server.address().port);
