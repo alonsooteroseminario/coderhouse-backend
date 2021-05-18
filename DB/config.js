9cc9f4ef7e34ffa848e8eb50afca9d1002415024
Luis Otero <alonsooteroseminario@gmail.com>
2021-05-18 17:49:53 -0400

    se cambio fs por sqlite3 para el chat de mensajes

---
+++ DB/config.js
@@ -0,0 +1,16 @@
+const mysql = {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'productos'
+    }
+  }
+const sqlite3 = {
+    client: 'sqlite3',
+    connection: { filename: './DB/mensajes.sqlite' },
+    useNullAsDefault: true
+}
+  
+module.exports = { mysql, sqlite3 };
