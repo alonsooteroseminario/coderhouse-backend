cb733c738a177406091925a77923be68860672d8
Luis Otero <alonsooteroseminario@gmail.com>
2021-05-20 11:32:09 -0400

    draft4

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,6 +1,7 @@
 // import express from 'express';
 // import Product from '../controllers/product';
 const express = require('express');
+const { convertToObject } = require('typescript');
 const router = express.Router();
 const { mysql:configMysql } = require('../../DB/config');
 const ProductoDB = require('../../DB/productoDb');
@@ -8,11 +9,18 @@ const productoDB = new ProductoDB(configMysql);
 const Product = require("../controllers/product");
 const product = new Product();
 
+const products = productoDB.listar()
+if (products === null) {
+  productoDB.crearTabla();
+}
+
+
+
 router.get("/", (req, res) => {
-  const products = product.get()
-  // const products = productoDB.crearTabla().then(() => {
-  //   return productoDB.listar();
-  // })
+  // const products = product.get()
+  const products = productoDB.listar().then((listado) => {
+      console.table(listado)
+    })
   res.render('vista', {
     active: "vista",
     products: products
@@ -20,10 +28,8 @@ router.get("/", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-    const products = product.get()
-    // const products = productoDB.crearTabla().then(() => {
-    //   return productoDB.listar();
-    // })
+    // const products = product.get()
+    const products = productoDB.listar();
     if (!products) {
       return res.status(404).json({
         error: "no hay productos cargados",
@@ -34,8 +40,8 @@ router.get("/", (req, res) => {
   
 router.get("/:id", (req, res) => {
     const { id } = req.params;
-    const currentProduct = product.getById(id)
-    // const currentProduct = productoDB.listarPorId(id);
+    // const currentProduct = product.getById(id)
+    const currentProduct = productoDB.listarPorId(id);
     if (currentProduct) {
       return res.json(currentProduct);
     }
@@ -46,46 +52,55 @@ router.get("/:id", (req, res) => {
   
 router.post("/", (req, res) => {
     const data = req.body;
-    const products = product.get()
-    if(product.add(data)) {
-      if (data.form === "1") return res.redirect('http://localhost:8080/nuevo-producto');
-      res.status(201).json(data);
-      res.render('nuevo-producto', {
-        products: products
-      })
-    }
-    // const products = productoDB.crearTabla().then(() => {
-    //   return productoDB.listar();
-    // })
-    // if(productoDB.insertar(data)) {
+    // const products = product.get()
+    // if(product.add(data)) {
     //   if (data.form === "1") return res.redirect('http://localhost:8080/nuevo-producto');
     //   res.status(201).json(data);
     //   res.render('nuevo-producto', {
     //     products: products
     //   })
     // }
+    const products = productoDB.listar();
+    console.log(products);
+    delete data.form;
+    data.id = products.length + 1;
+    const nuevoObjeto = {
+      id: data.id,
+      title: data.title,
+      price: data.price,
+      thumbnail: data.thumbnail
+    }
+    console.log(nuevoObjeto);
+
+    if(productoDB.insertar(nuevoObjeto)) {
+      res.redirect('http://localhost:8080/nuevo-producto');
+      res.status(201).json(nuevoObjeto);
+      res.render('nuevo-producto', {
+        products: products
+      })
+    }
     res.status(400).send();
   });
   
 router.put("/:id", (req, res) => {
     const data = req.body;
     const { id } = req.params;
-    if(product.update(id, data)) {
-      res.status(201).json(data);
-    }
-    // if(productoDB.actualizarPorId(id, data)) {
+    // if(product.update(id, data)) {
     //   res.status(201).json(data);
     // }
+    if(productoDB.actualizarPorId(id, data)) {
+      res.status(201).json(data);
+    }
     res.status(400).send();
   });
   
 router.delete("/:id", (req, res) => {
     const { id } = req.params;
-    const currentProduct = product.getById(id)
-    // const currentProduct = productoDB.listarPorId(id);
+    // const currentProduct = product.getById(id)
+    // product.remove(id);
+    const currentProduct = productoDB.listarPorId(id);
+    productoDB.borrarPorId(id);
     res.json(currentProduct);
-    product.remove(id);
-    // productoDB.borrarPorId(id);
   });
 
 module.exports = router;
