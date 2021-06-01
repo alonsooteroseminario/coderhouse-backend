dd7f3016c3d6d5ce53fa53eb3a9a92a8bcf01780
Luis Otero <alonsooteroseminario@gmail.com>
2021-05-31 20:23:55 -0400

    agregado productos, actualizado lunes por la noche

--- DB/productoDb.js
+++ DB/productoDb.js
@@ -1,40 +1,88 @@
 // import knex from 'knex'
-const knex = require('knex');
+// const knex = require('knex');
 const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/ecommerce';
 
+const esquemaProducto = new mongoose.Schema({
+  id: { type: Number, require: true },
+  title: { type: String, require: true, max: 100 },
+  price: { type: String, require: true, max: 100 },
+  thumbnail: { type: String, require: true, max: 100 }
+})
+
+const daoProductos = mongoose.model('productos', esquemaProducto);
+
 class ProductoDB {
   constructor(config) {
-    this.knex = knex(config)
+    // this.knex = knex(config)
+    mongoose.connect(url,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    }, (err) => {
+      if (err) {
+        console.log(err);
+      }else{
+        console.log('Conectado a la base en constructor');
+      }
+    })
   }
 
   crearTabla() {
-    return this.knex.schema.dropTableIfExists('productos')
-      .then(() => {
-        return this.knex.schema.createTable('productos', table => {
-          table.increments('id').primary();
-          table.string('title').notNullable();
-          table.float('price');
-          table.integer('thumbnail');
-        })
-      })
+    // return this.knex.schema.dropTableIfExists('productos')
+    //   .then(() => {
+    //     return this.knex.schema.createTable('productos', table => {
+    //       table.increments('id').primary();
+    //       table.string('title').notNullable();
+    //       table.float('price');
+    //       table.integer('thumbnail');
+    //     })
+    //   })
   }
 
-  insertar(productos) {
-    return this.knex('productos').insert(productos)
+  insertar(producto) {
+    return daoProductos.create(producto, (err,res) => {
+      if (err) {
+        console.log(err);
+      }else{
+        console.log(res);
+      }
+    });
+    // return this.knex('productos').insert(productos)
   }
 
   listar() {
-    return this.knex('productos').select()
+    return daoProductos.find({}, (err,res) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(res)
+      }
+    });
+    // return this.knex('productos').select()
   }
 
   listarPorId(id) {
-    return this.knex.from('productos').where('id', id).select()
+    return daoProductos.find({id: id}, (err,res) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(res)
+      }
+    });
+    // return this.knex.from('productos').where('id', id).select()
   }
 
   borrarPorId(id) {
-    return this.knex.from('productos').where('id', id).del()
+    return daoProductos.deleteOne({id: id}, (err,res) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(res)
+      }
+    });
+    // return this.knex.from('productos').where('id', id).del()
   }
   actualizarPorId(id, data) {
 
@@ -42,14 +90,27 @@ class ProductoDB {
     const nuevoPrice = data.price;
     const nuevothumbnail = data.thumbnail;
 
-    return this.knex.from('productos').where('id', id).update({
+    return daoProductos.updateOne({id: id}, {$set: {
       title: nuevoTitle,
       price: nuevoPrice,
       thumbnail: nuevothumbnail
-    })
+    }}, (err,res) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(res)
+      }
+    });
+
+    // return this.knex.from('productos').where('id', id).update({
+    //   title: nuevoTitle,
+    //   price: nuevoPrice,
+    //   thumbnail: nuevothumbnail
+    // })
   }
   cerrar() {
-    return this.knex.destroy()
+    mongoose.disconnect(err => { console.log('desconectado de la base') });
+    // return this.knex.destroy()
   }
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -8,15 +8,12 @@ const productoDB = new ProductoDB(configMysql);
 const Product = require("../controllers/product");
 const product = new Product();
 
-router.get("/", (req, res) => {
-  const products = product.get()
-  productoDB.crearTabla().then(() => {
-    return productoDB.insertar(products)
-  }).then(() => {
-    return productoDB.listar()
-  }).then((listado) => {
-    console.table(listado)
-  })
+router.get("/", async (req, res) => {
+  const products = product.get();
+  await productoDB.insertar(products);
+  const listado = await productoDB.listar();
+  console.table(listado);
+
   res.render('vista', {
     active: "vista",
     products: products
@@ -41,7 +38,7 @@ router.get("/:id", (req, res) => {
     });
   });
   
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const data = req.body;
     const products = product.get()
     if(product.add(data)) {
@@ -50,26 +47,26 @@ router.post("/", (req, res) => {
       res.render('nuevo-producto', {
         products: products
       })
-      productoDB.insertar(products)
+      await productoDB.insertar(products)
     }
     res.status(400).send();
   });
   
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
     const data = req.body;
     const { id } = req.params;
     if(product.update(id, data)) {
-      productoDB.actualizarPorId(id, data)
+      await productoDB.actualizarPorId(id, data)
       res.status(201).json(data);
     }
     res.status(400).send();
   });
   
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
     const { id } = req.params;
     const currentProduct = product.getById(id)
     product.remove(id);
-    productoDB.borrarPorId(id);
+    await productoDB.borrarPorId(id);
     res.json(currentProduct);
   });
 
